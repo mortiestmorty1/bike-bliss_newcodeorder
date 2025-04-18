f1e09c37d9691773bc18bf52093281f4a32a0e1f
mortiestmorty1 <shoaibahmed@Shoaibs-MacBook-Pro.local>
2025-04-18 17:09:39 +0500

    deploying

--- app/api/auth/[...nextauth]/config.ts
+++ app/api/auth/[...nextauth]/config.ts
@@ -31,7 +31,22 @@ export const authOptions: AuthOptions = {
     signIn: "/login",
   },
   session: {
-    strategy: "jwt" as const,
+    strategy: "jwt",
+    maxAge: 30 * 24 * 60 * 60, // 30 days
+  },
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (session.user) {
+        session.user.id = token.id as string;
+      }
+      return session;
+    }
   },
   secret: process.env.NEXTAUTH_SECRET,
 }; 
